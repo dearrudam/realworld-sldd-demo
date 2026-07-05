ea0aad28861e75dde2c983ab333666e52d3c7227
Maximillian Arruda <dearrudam@gmail.com>
2026-07-05 00:40:06 -0300

    refactor(service): rename `airhacks.qmp` package to `dearrudam.realworld` and remove unused modules
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ service/src/main/java/dearrudam/realworld/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Business components are organized as children of this package, following the BCE pattern
+ * with boundary, control, and entity packages for each domain responsibility.
+ */
+package dearrudam.realworld;
